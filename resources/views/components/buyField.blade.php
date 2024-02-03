4c7614358d37fce037ed3d82962ab0f1f2185988
Christian Milanes <christianmilanes01@gmail.com>
2024-02-03 18:01:08 +0100

    buyField component created and implemented

---
+++ resources/views/components/buyField.blade.php
@@ -0,0 +1,11 @@
+<div class="row g-3 justify-content-center pe-5 ps-5 align-items-center">
+  <div class="col-auto me-auto lab">
+    <label for="menor4" class="col-form-label fw-bold">{{ $fieldName }}</label>
+  </div>
+  <div class="col-auto">
+    <p class="fw-medium m-0">Nro. entradas: </p>
+  </div>
+  <div class="col-auto">
+    <input type="number" min="0" max="5" value="0" id="menor4" class="form-control numSales">
+  </div>
+</div>
--- resources/views/includes/secondCard.blade.php
+++ resources/views/includes/secondCard.blade.php
@@ -2,100 +2,35 @@
 <x-wrapper>
   <x-slot name="wTitle">Seleccione entradas que desea comprar.</x-slot>
   <!-- Campo 1 -->
-  <div class="row g-3 justify-content-center pe-5 ps-5 align-items-center">
-    <div class="col-auto me-auto lab">
-      <label for="menor4" class="col-form-label fw-bold">Menores de cuatro años (Gratis):</label>
-    </div>
-    <div class="col-auto">
-      <p class="fw-medium m-0">Nro. entradas: </p>
-    </div>
-    <div class="col-auto">
-
-      <input type="number" min="0" max="5" value="0" id="menor4" class="form-control numSales">
-    </div>
-  </div>
+  <x-buyField>
+    <x-slot name="fieldName">Personas con discapacidad (Gratis):</x-slot>
+  </x-buyField>
   <!-- Campo 2 -->
-  <div class="row g-3 justify-content-center pe-5 ps-5 align-items-center">
-    <div class="col-auto me-auto lab">
-      <label for="menor4" class="col-form-label fw-bold">Personas sin empleo (Gratis):</label>
-    </div>
-    <div class="col-auto">
-      <p class="fw-medium m-0">Nro. entradas: </p>
-    </div>
-    <div class="col-auto">
-      <input type="number" min="0" max="5" value="0" id="menor4" class="form-control numSales">
-    </div>
-  </div>
+  <x-buyField>
+    <x-slot name="fieldName">Menores de cuatro años (Gratis):</x-slot>
+  </x-buyField>
   <!-- Campo 3 -->
-  <div class="row g-3 justify-content-center pe-5 ps-5 align-items-center">
-    <div class="col-auto me-auto lab">
-      <label for="menor4" class="col-form-label fw-bold">Personas con discapacidad (Gratis):</label>
-    </div>
-    <div class="col-auto">
-      <p class="fw-medium m-0">Nro. entradas: </p>
-    </div>
-    <div class="col-auto">
-      <input type="number" min="0" max="5" value="0" id="menor4" class="form-control numSales">
-    </div>
-  </div>
+  <x-buyField>
+    <x-slot name="fieldName">Personas sin empleo (Gratis):</x-slot>
+  </x-buyField>
   <!-- Campo 4 -->
-  <div class="row g-3 justify-content-center pe-5 ps-5 align-items-center">
-    <div class="col-auto me-auto lab">
-      <label for="menor4" class="col-form-label fw-bold">Menores entre 4 y 12 años (3€):</label>
-    </div>
-    <div class="col-auto">
-      <p class="fw-medium m-0">Nro. entradas: </p>
-    </div>
-    <div class="col-auto">
-      <input type="number" min="0" max="5" value="0" id="menor4" class="form-control numSales">
-    </div>
-  </div>
+  <x-buyField>
+    <x-slot name="fieldName">Menores entre 4 y 12 años (3€):</x-slot>
+  </x-buyField>
   <!-- Campo 5 -->
-  <div class="row g-3 justify-content-center pe-5 ps-5 align-items-center">
-    <div class="col-auto me-auto lab">
-      <label for="menor4" class="col-form-label fw-bold">Mayores de 65 años (3€):</label>
-    </div>
-    <div class="col-auto">
-      <p class="fw-medium m-0">Nro. entradas: </p>
-    </div>
-    <div class="col-auto">
-      <input type="number" min="0" max="5" value="0" id="menor4" class="form-control numSales">
-    </div>
-  </div>
+  <x-buyField>
+    <x-slot name="fieldName">Mayores de 65 años (3$):</x-slot>
+  </x-buyField>
   <!-- Campo 6 -->
-  <div class="row g-3 justify-content-center pe-5 ps-5 align-items-center">
-    <div class="col-auto me-auto lab">
-      <label for="menor4" class="col-form-label fw-bold">Estudieantes (3€):</label>
-    </div>
-    <div class="col-auto">
-      <p class="fw-medium m-0">Nro. entradas: </p>
-    </div>
-    <div class="col-auto">
-      <input type="number" min="0" max="5" value="0" id="menor4" class="form-control numSales">
-    </div>
-  </div>
+  <x-buyField>
+    <x-slot name="fieldName">Estudieantes (3$):</x-slot>
+  </x-buyField>
   <!-- Campo 7 -->
-  <div class="row g-3 justify-content-center pe-5 ps-5 align-items-center">
-    <div class="col-auto me-auto lab">
-      <label for="menor4" class="col-form-label fw-bold">Profesores (3€):</label>
-    </div>
-    <div class="col-auto">
-      <p class="fw-medium m-0">Nro. entradas: </p>
-    </div>
-    <div class="col-auto">
-      <input type="number" min="0" max="5" value="0" id="menor4" class="form-control numSales">
-    </div>
-  </div>
+  <x-buyField>
+    <x-slot name="fieldName">Profesores (3$):</x-slot>
+  </x-buyField>
   <!-- Campo 8 -->
-  <div class="row g-3 justify-content-center pe-5 ps-5 align-items-center">
-    <div class="col-auto me-auto lab">
-      <label for="menor4" class="col-form-label fw-bold">Adultos (7$):</label>
-    </div>
-    <div class="col-auto">
-      <p class="fw-medium m-0">Nro. entradas: </p>
-    </div>
-    <div class="col-auto">
-      <input type="number" min="0" max="5" value="0" id="menor4" class="form-control numSales">
-    </div>
-  </div>
+  <x-buyField>
+    <x-slot name="fieldName">Adultos (7$):</x-slot>
+  </x-buyField>
 </x-wrapper>
