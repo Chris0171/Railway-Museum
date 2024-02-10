74678c2c29da4dbb2a22c2e787b6cd0060c6f530
Christian Milanes <christianmilanes01@gmail.com>
2024-02-10 22:33:50 +0100

    getTicketExtraData private function created

--- app/Http/Controllers/ticketsController.php
+++ app/Http/Controllers/ticketsController.php
@@ -10,8 +10,49 @@ class TicketsController extends Controller
     public function tickets_save(Request $request)
     {
         $datos = $request->input("tickets");
-        dump($datos);
+        dump($datos, $request->input("visitDate"));
+        dump($request->input("franja"));
         // Creando instancias de clases
         //$tickets = new Tickets();
+    }
+    private function getTicketExtraData($id)
+    {
+        $data = array();
+        switch ($id) {
+            case "0":
+                $data[] = "DISCAPACITADO";
+                $data[] = 0.00;
+                break;
+            case "1":
+                $data[] = "MENOR4";
+                $data[] = 0.00;
+                break;
+            case "2":
+                $data[] = "DESEMPLEADO";
+                $data[] = 0.00;
+                break;
+            case "3":
+                $data[] = "ENTRE4Y12";
+                $data[] = 3.00;
+                break;
+            case "4":
+                $data[] = "MAYOR65";
+                $data[] = 3.00;
+                break;
+            case "5":
+                $data[] = "ESTUDIANTE";
+                $data[] = 3.00;
+                break;
+            case "6":
+                $data[] = "PROFESOR";
+                $data[] = 3.00;
+                break;
+            case "7":
+                $data[] = "ADULTO";
+                $data[] = 7.00;
+
+                break;
+        }
+        return $data;
     }
 }
