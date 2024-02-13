22108d8835d69ade4163607c4a72ac94f95ccb60
Christian Milanes <christianmilanes01@gmail.com>
2024-02-13 12:41:55 +0100

    change inputs name and update script

--- resources/views/includes/firstCard.blade.php
+++ resources/views/includes/firstCard.blade.php
@@ -20,21 +20,21 @@
         </div>
         <div class="col-auto p-0">
             <div class="form-check">
-                <input type="radio" class="btn-check" name="franja" id="franja1" value="10:00">
+                <input type="radio" class="btn-check" name="timeSlot" id="franja1" value="10:00-13:00">
                 <label class="btn border-dark border-2 btn-outline-dark fw-bold rounded-4" for="franja1">10:00 a
                     13:00</label>
             </div>
         </div>
         <div class="col-auto p-0">
             <div class="form-check">
-                <input type="radio" class="btn-check" name="franja" id="franja2" value="13:00">
+                <input type="radio" class="btn-check" name="timeSlot" id="franja2" value="13:00-15:00">
                 <label class="btn border-dark border-2 btn-outline-dark fw-bold rounded-4" for="franja2">13:00 a
                     15:00</label>
             </div>
         </div>
         <div class="col-auto p-0">
             <div class="form-check">
-                <input type="radio" class="btn-check" name="franja" id="franja3" value="15:00">
+                <input type="radio" class="btn-check" name="timeSlot" id="franja3" value="15:00-18:00">
                 <label class="btn border-dark border-2 btn-outline-dark fw-bold rounded-4" for="franja3">15:00 a 18:00
                 </label>
             </div>
