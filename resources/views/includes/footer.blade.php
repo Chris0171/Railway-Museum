5001e08c6d520f23a9907bbeab3670b766fb59b6
Christian Milanes <christianmilanes01@gmail.com>
2024-02-14 11:31:28 +0100

    new content added

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,13 +1,40 @@
-<footer class="bg-aux-1 mt-5 p-3">
-  <div class="row justify-content-center align-items-center">
-    <div class="col-10 col-md-4 text-center">
-      <p>aquí un texto</p>
+<footer>
+    <div class="bg-footer m-0 mt-5 p-3">
+        <div class="row justify-content-center">
+            <div class="col-10 col-md-3 text-center align-self-center">
+                <img class="img-fluid" style="width: 200px" src="/images/Railway-Museum-logo.png"
+                    alt="Railway-Museum-logo.png">
+            </div>
+            <div class="col-10 col-md-3 p-3 pe-5">
+                <h5 class="text-white">SECCIONES</h5>
+                <hr class="border border-1 opacity-100">
+                <a href="/ticketsSale" class="link">Venta de entradas</a><br>
+                <a href="/" class="link">Página principal</a>
+            </div>
+            <div class="col-10 col-md-3 p-3 pe-5">
+                <h5 class="text-white">VISITA</h5>
+                <hr class="border border-1 opacity-100">
+                <a href="https://www.museodelferrocarril.org/visita/index.asp" class="link">Horarios y tarifas</a><br>
+                <a href="https://www.museodelferrocarril.org/visita/datos_practicos.asp" class="link">Cómo
+                    llegar</a><br>
+                <a href="https://www.museodelferrocarril.org/visita/datos_practicos.asp" class="link">Plano del
+                    museo</a><br>
+                <a href="https://www.museodelferrocarril.org/comunicacion/contacto.asp" class="link"
+                    target="_blank">Contancta con el
+                    museo.</a>
+            </div>
+
+            <div class="col-10 col-md-3 p-3 pe-5">
+                <h5 class="text-white">REDES SOCIALES</h5>
+                <hr class="border border-1 opacity-100">
+                <a href="https://www.instagram.com/museoferrocarrilmadrid/" class="link">Instagram</a><br>
+                <a href="https://www.youtube.com/user/museoferrocarrilffe" class="link">Youtube</a><br>
+                <a href="https://twitter.com/m_ferrocarril" class="link">Twiter</a><br>
+                <a href="https://open.spotify.com/user/injjdxr0n3wvaqm5gk9lfaeki" class="link">Spotify</a>
+            </div>
+        </div>
     </div>
-    <div class="col-10 col-md-4 text-center">
-      <p>aquí redes sociales</p>
+    <div class="text-center bg-footer-1 p-3 text-white">
+        Copyright © 2024, Museo del Ferrocarril de Madrid - Pº Delicias 61 - 28045 Madrid - Telf: 91 539 00 85
     </div>
-    <div class="col-10 col-md-4 text-center">
-      <p>aquí otro texto</p>
-    </div>
-  </div>
 </footer>
--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,11 +1,12 @@
 <header class="bg-aux-1 mb-4">
-  <div class="row justify-content-center align-items-center p-2">
-    <div class="col-5 col-md-3">
-      <a href=""><img class="img-fluid" width="180px" src="images/Railway-Museum-logo.png"
-          alt="Railway-Museum-Logo"></a>
+    <div class="row justify-content-center align-items-center p-2">
+        <div class="col-5 col-md-3">
+            <a href="/"><img class="img-fluid" width="180px" src="/images/Railway-Museum-logo.png"
+                    alt="Railway-Museum-Logo"></a>
+        </div>
+        <div class="col-6 col-md-8 text-center">
+            <p class="h5 text-white fw-bold">El mayor museo de ferrocarril de España ahora abierto todos los dias de la
+                semana.</p>
+        </div>
     </div>
-    <div class="col-6 col-md-8 text-center">
-      <p class="h5 fw-bold">El mayor museo de ferrocarril de España ahora abierto todos los dias de la semana.</p>
-    </div>
-  </div>
 </header>
