85582157c8e0f9fb0c91ea8b5d98dfb47c1dbc8a
Christian Milanes <christianmilanes01@gmail.com>
2024-02-14 11:29:35 +0100

    new routes added

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,36 @@
 <?php
 
 use App\Http\Controllers\TicketsController;
+use App\Http\Controllers\ticketOfficeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
 });
+
 Route::get('/ticketsSale', function () {
     return view('ticketsSale');
 });
+
 route::get('/ticketOffice', function () {
     return view('ticketOffice');
 });
+
+route::post('/ticketsSale/buy', [TicketsController::class, 'tickets_save']);
+
 route::get('/login', function () {
     return view('login');
 });
-route::post('/ticketsSale/buy', [TicketsController::class, 'tickets_save']);
+
+route::post('/login/check', [ticketOfficeController::class, 'checkPass']);
+
+route::get('/login/{error}', function ($error) {
+    return view('login', ['error' => $error]);
+})->name('loginError');
+
+Route::get('/{success}', function ($success) {
+    return view('index', ['success' => $success]);
+})->name('buySuccess');
 
 Route::fallback(function () {
     return view('error404');
