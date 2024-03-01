9ee4f05a6754dff7c5d561dbb4aee8d2735ee674
Christian Milanes <christianmilanes01@gmail.com>
2024-03-01 11:57:09 +0100

    ticketsOffice controller finished

--- app/Http/Controllers/ticketOfficeController.php
+++ app/Http/Controllers/ticketOfficeController.php
@@ -4,11 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Auth;
 
 class ticketOfficeController extends Controller
 {
     public function checkPass(Request $request)
     {
-        return Redirect::route('loginError', ['error' => "2"]);
+        $credentials = $request->only('email', 'password');
+
+        // Verificar las credenciales
+        if (Auth::attempt($credentials)) {
+            // La contraseña es correcta, el usuario está autenticado
+            return redirect()->route('ticketOffice'); // Cambia 'dashboard' por la ruta a la que quieres redirigir
+        } else {
+            // La contraseña es incorrecta
+            return Redirect::route('loginError', ['error' => "2"]); // Redirigir de vuelta con un mensaje de error
+        }
     }
 }
